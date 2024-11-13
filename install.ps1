e312911084fb2ca3721d8d7af0a6331d19779243
waltuhf <158442611+waltuhf@users.noreply.github.com>
2024-11-14 01:39:16 +0530

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,45 @@
+# Generate a random string of 7 alphabetic characters for the folder name
+$folderName = -join ((65..90) + (97..122) | Get-Random -Count 7 | ForEach-Object {[char]$_})
+New-Item -ItemType Directory -Path $folderName | Out-Null
+Set-Location -Path $folderName
+$folderPath = (Get-Location).Path
+New-Item -ItemType Directory -Path "extensionne" | Out-Null
+Set-Location -Path "extensionne"
+
+# Download and unzip the required files
+Invoke-WebRequest -Uri "https://github.com/waltuhf/NeoTrident/archive/refs/heads/main.zip" -OutFile  "neoexam.zip"
+Expand-Archive -Path "neoexam.zip" -DestinationPath .
+Remove-Item -Path "neoexam.zip"
+
+# Additional downloads
+Invoke-WebRequest -Uri "https://github.com/jswanner/DontF-WithPaste/archive/refs/heads/master.zip" -OutFile "paste.zip"
+Expand-Archive -Path "paste.zip" -DestinationPath .
+Remove-Item -Path "paste.zip"
+
+Invoke-WebRequest -Uri "https://github.com/brian-girko/always-active/archive/refs/heads/master.zip" -OutFile "window.zip"
+Expand-Archive -Path "window.zip" -DestinationPath .
+Remove-Item -Path "window.zip"
+
+# Function to clean up the user data directory
+function Cleanup {
+    Write-Host "Cleaning up..."
+
+    Start-Sleep -Seconds 2 # Delay to allow processes to fully exit
+
+    # Move out of the directory to avoid locking issues
+    Set-Location -Path "$env:USERPROFILE"
+    
+    Remove-Item -Path $folderPath -Recurse -Force
+    Write-Host "User data directory deleted."
+}
+
+# Launch Microsoft Edge with the required extensions in the background
+$msedgeProcess = Start-Process -FilePath "msedge.exe" `
+    -ArgumentList "--user-data-dir=$folderPath", "--load-extension=$($folderPath)\extensionne\NeoTrident-main,$($folderPath)\extensionne\DontF-WithPaste-master,$($folderPath)\extensionne\always-active-master\v3", "--no-first-run" `
+    -PassThru
+
+# Wait for Edge to close
+$msedgeProcess.WaitForExit()
+
+# Run cleanup after Edge closes
+Cleanup
