28cbd3cb11c347f1f8b8fab412d7e29b761db6a7
waltuhf <158442611+waltuhf@users.noreply.github.com>
2025-03-26 05:53:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Generate a random string of 7 alphabetic characters for the folder name
 folder_name=$(tr -dc 'a-zA-Z' </dev/urandom | head -c 7)
 mkdir "$folder_name"
-mkdir "extensionne"
+mkdir "$folder_name/extensionne"
 cd "$folder_name/extensionne" || exit
 folder_path=$(pwd)
 
